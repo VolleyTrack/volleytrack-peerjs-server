37939d79f22e2a6f9af9611b00508be0cd92a194
VolleyTrack <support@volleytrack.app>
2026-06-09 16:11:42 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { PeerServer } = require('peer')
+const PORT = process.env.PORT || 9000
+const server = PeerServer({
+  port: PORT,
+  path: '/peerjs',
+  allow_discovery: false,
+  corsOptions: {
+    origin: ['https://volleytrack.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+})
+server.on('connection', client => console.log(`[+] Connected: ${client.getId()}`))
+server.on('disconnect', client => console.log(`[-] Disconnected: ${client.getId()}`))
+console.log(`PeerJS server running on port ${PORT}`)
